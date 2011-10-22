21bbea368a96a613fddefdb4fbe464f2a44bf88e
Shlomi Fish <shlomif@iglu.org.il>
2011-10-22 12:02:16 +0200

    Add static.

--- patsolve/patmain.c
+++ patsolve/patmain.c
@@ -29,7 +29,7 @@
 #include "pat.h"
 #include "tree.h"
 
-const char Usage[] =
+static const char Usage[] =
   "usage: %s [-s|f] [-k|a] [-w<n>] [-t<n>] [-E] [-S] [-q|v] [layout]\n"
   "-s Seahaven (same suit), -f Freecell (red/black)\n"
   "-k only Kings start a pile, -a any card starts a pile\n"
