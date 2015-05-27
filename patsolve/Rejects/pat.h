0e9e3814726d35eb03e48b42a6986f708f3c9134
Shlomi Fish <shlomif@shlomifish.org>
2015-05-27 19:15:24 +0300

    Remove some unused defs to Rejects.

---
+++ patsolve/Rejects/pat.h
@@ -0,0 +1,6 @@
+
+#define FCS_PATS__ACE  1
+#define FCS_PATS__CLUB      0x01         /* black */
+#define FCS_PATS__DIAMOND   0x02         /* red */
+#define FCS_PATS__HEART     0x00         /* red */
+#define FCS_PATS__SPADE     0x03         /* black */
