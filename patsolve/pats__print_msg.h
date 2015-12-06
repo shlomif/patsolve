5075d4dc0cdd3af7d9abd9aa5a1d751ffc9055ea
Shlomi Fish <shlomif@shlomifish.org>
2015-12-06 21:46:21 +0200

    Extract into a common header.

---
+++ patsolve/pats__print_msg.h
@@ -0,0 +1,61 @@
+/* Copyright (c) 2002 Tom Holroyd
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef FC_SOLVE_PATSOLVE_PATS_PRINT_MSG_H
+#define FC_SOLVE_PATSOLVE_PATS_PRINT_MSG_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include "config.h"
+
+static void print_msg(const char *msg, ...)
+{
+    va_list ap;
+
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    va_end(ap);
+}
+
+static char *Progname = NULL;
+
+/* Print a message and exit. */
+static void fatalerr(const char *msg, ...)
+{
+    va_list ap;
+
+    if (Progname) {
+        fprintf(stderr, "%s: ", Progname);
+    }
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    va_end(ap);
+    fputc('\n', stderr);
+
+    exit(1);
+}
+
+#define USAGE() print_msg(Usage, Progname)
+
+#endif /* FC_SOLVE_PATSOLVE_PATS_PRINT_MSG_H */
