b884eb4443a45097483de23983768998533864f8
Shlomi Fish <shlomif@shlomifish.org>
2016-12-04 02:26:10 +0200

    [AppVeyor] Try to fix - shell booboo

--- CI-testing/continuous-integration-testing.pl
+++ CI-testing/continuous-integration-testing.pl
@@ -34,7 +34,9 @@ local $ENV{RUN_TESTS_VERBOSE} = 1;
 my $CWD = getcwd();
 mkpath("B");
 chdir("B");
-do_system({cmd => ["cmake " . (defined($cmake_gen) ? qq#-G "$cmake_gen"# : "") . qq# -DFC_SOLVE_SRC_PATH="$CWD${SEP}fc-solve${SEP}fc-solve${SEP}source" ../patsolve#]});
+do_system({cmd => ["cmake " . (defined($cmake_gen) ? qq#-G "$cmake_gen"# : "")
+            . (defined($ENV{CMAKE_MAKE_PROGRAM}) ? "-DCMAKE_MAKE_PROGRAM=$ENV{CMAKE_MAKE_PROGRAM}" : ())
+            . qq# -DFC_SOLVE_SRC_PATH="$CWD${SEP}fc-solve${SEP}fc-solve${SEP}source" ../patsolve#]});
 do_system({cmd => [$MAKE]});
 do_system({cmd => [$MAKE, 'check',]});
 chdir($CWD);
