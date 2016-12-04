593e38b8a5029407eeb15df8115fd9b17d45fd33
Shlomi Fish <shlomif@shlomifish.org>
2016-12-04 02:41:35 +0200

    tidy

--- CI-testing/continuous-integration-testing.pl
+++ CI-testing/continuous-integration-testing.pl
@@ -14,29 +14,39 @@ sub do_system
 
     my $cmd = $args->{cmd};
     print "Running [@$cmd]";
-    if (system(@$cmd))
+    if ( system(@$cmd) )
     {
         die "Running [@$cmd] failed!";
     }
 }
 
-my $IS_WIN = ($^O eq "MSWin32");
-my $SEP = $IS_WIN ? "\\" : '/';
-my $MAKE = $IS_WIN ? 'gmake' : 'make';
+my $IS_WIN = ( $^O eq "MSWin32" );
+my $SEP    = $IS_WIN ? "\\" : '/';
+my $MAKE   = $IS_WIN ? 'gmake' : 'make';
 
 my $cmake_gen;
-GetOptions(
-    'gen=s' => \$cmake_gen,
-) or die 'Wrong options';
+GetOptions( 'gen=s' => \$cmake_gen, )
+    or die 'Wrong options';
 
 local $ENV{RUN_TESTS_VERBOSE} = 1;
 
 my $CWD = getcwd();
 mkpath("B");
 chdir("B");
-do_system({cmd => ["cmake " . (defined($cmake_gen) ? qq#-G "$cmake_gen"# : "")
-            . ' ' . (defined($ENV{CMAKE_MAKE_PROGRAM}) ? "-DCMAKE_MAKE_PROGRAM=$ENV{CMAKE_MAKE_PROGRAM}" : ())
-            . qq# -DFC_SOLVE_SRC_PATH="$CWD${SEP}fc-solve${SEP}fc-solve${SEP}source" ../patsolve#]});
-do_system({cmd => [$MAKE]});
-do_system({cmd => [$MAKE, 'check',]});
+do_system(
+    {
+        cmd => [
+                  "cmake "
+                . ( defined($cmake_gen) ? qq#-G "$cmake_gen"# : "" ) . ' '
+                . (
+                defined( $ENV{CMAKE_MAKE_PROGRAM} )
+                ? "-DCMAKE_MAKE_PROGRAM=$ENV{CMAKE_MAKE_PROGRAM}"
+                : ()
+                )
+                . qq# -DFC_SOLVE_SRC_PATH="$CWD${SEP}fc-solve${SEP}fc-solve${SEP}source" ../patsolve#
+        ]
+    }
+);
+do_system( { cmd => [$MAKE] } );
+do_system( { cmd => [ $MAKE, 'check', ] } );
 chdir($CWD);
