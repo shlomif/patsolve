bd07f065b5c26b8cde3927058ece967f7933b016
Shlomi Fish <shlomif@shlomifish.org>
2019-12-27 19:33:55 +0200

    apptravis fix #2 - rinutils

--- CI-testing/continuous-integration-testing.pl
+++ CI-testing/continuous-integration-testing.pl
@@ -26,12 +26,54 @@ my $IS_WIN = ( $^O eq "MSWin32" );
 my $CMAKE_SAFE_SEPARATOR_ON_WINDOWS = '/';
 my $SEP  = $IS_WIN ? $CMAKE_SAFE_SEPARATOR_ON_WINDOWS : '/';
 my $MAKE = $IS_WIN ? 'gmake' : 'make';
+my $SUDO = $IS_WIN ? '' : 'sudo';
 
 my $cmake_gen;
 GetOptions( 'gen=s' => \$cmake_gen, )
     or die 'Wrong options';
 
 local $ENV{RUN_TESTS_VERBOSE} = 1;
+if ( !$ENV{SKIP_RINUTILS_INSTALL} )
+{
+    do_system(
+        {
+            cmd => [ qw#git clone https://github.com/shlomif/rinutils#, ]
+        }
+    );
+    do_system(
+        {
+            cmd => [
+                      qq#cd rinutils && mkdir B && cd B && cmake #
+                    . ( defined($cmake_gen) ? qq# -G "$cmake_gen" # : "" )
+                    . (
+                    defined( $ENV{CMAKE_MAKE_PROGRAM} )
+                    ? " -DCMAKE_MAKE_PROGRAM=$ENV{CMAKE_MAKE_PROGRAM} "
+                    : ""
+                    )
+                    . ( $IS_WIN ? " -DCMAKE_INSTALL_PREFIX=C:/foo " : '' )
+                    . qq# .. && $SUDO $MAKE install#
+            ]
+        }
+    );
+}
+my $CMAKE_MODULE_PATH = join ";",
+    (
+    map { ; $_, s%/%\\%gr } (
+        map { ; $_, "$_/Rinutils", "$_/Rinutils/cmake" }
+            map { ; $_, "$_/lib", "$_/lib64" } ( map { ; "c:$_", $_ } ("/foo") )
+    )
+    );
+
+# die "<$CMAKE_MODULE_PATH>";
+if ($IS_WIN)
+{
+    ( $ENV{CMAKE_MODULE_PATH} //= '' ) .= ";$CMAKE_MODULE_PATH;";
+
+    # ( $ENV{PKG_CONFIG_PATH} //= '' ) .= ";C:\\foo\\lib\\pkgconfig;";
+    ( $ENV{PKG_CONFIG_PATH} //= '' ) .=
+        ";/foo/lib/pkgconfig/;/c/foo/lib/pkgconfig/";
+    $ENV{RINUTILS_INCLUDE_DIR} = "C:/foo/include";
+}
 
 my $CWD = Path::Tiny->cwd;
 path("B")->mkpath;
